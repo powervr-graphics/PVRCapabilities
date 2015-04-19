c15059472b6d572d5349a1db3884fe2ed9292302
Joe Davis <joemdavis@gmail.com>
2015-04-19 15:06:16 +0100

    Refactor: Removed Rogue specific naming (may add SGX data soon) Refactor: Tweaked variable names and aligned GPU model list

--- PVRCapabilities.hpp
+++ PVRCapabilities.hpp
@@ -4,100 +4,98 @@
 #include <unordered_map>
 
 namespace pvr {
-enum ERogueGeneration {
-	PVR_SERIES6,
-	PVR_SERIES6XE,
-	PVR_SERIES6XT,
-	PVR_SERIES7XE,
-	PVR_SERIES7XT,
-	PVR_SERIES_UNKNOWN
+enum class Family {
+	SERIES6,
+	SERIES6XE,
+	SERIES6XT,
+	SERIES7XE,
+	SERIES7XT,
+	UNKNOWN
 };
-enum ECapabilityFlags {
-	PVR_CAP_FP16            = 1 << 1,        // USC has 16-bit float units
-	PVR_CAP_RAYTRACING      = 1 << 2,        // Accelerated ray tracing
+enum EFlags {
+	CAP_FP16       = 1 << 1,        // USC has 16-bit float units
+	CAP_RAYTRACING = 1 << 2,        // Accelerated ray tracing
 };
 struct SCapabilities {
-	ERogueGeneration generation;             // Rogue variant
-	unsigned int fp16_operations_per_clock;  // Peak operations per-clock (FP16)
-	unsigned int fp32_operations_per_clock;  // Peak operations per-clock (FP32)
-	unsigned int bilinear_samples_per_clock; // Peak bilinear samples per-clock
-	unsigned int flags;                      // Capability flags
+	Family family_;                 // PowerVR variant
+	unsigned int fp16_ops_;         // Peak operations per-clock (FP16)
+	unsigned int fp32_ops_;         // Peak operations per-clock (FP32)
+	unsigned int bilinear_samples_; // Peak bilinear samples per-clock
+	unsigned int flags_;            // Capability flags
 
 	SCapabilities()
 	:
-	generation(PVR_SERIES_UNKNOWN),
-	fp16_operations_per_clock(0),
-	fp32_operations_per_clock(0),
-	bilinear_samples_per_clock(0),
-	flags(0)
+    family_(Family::UNKNOWN),
+	fp16_ops_(0),
+	fp32_ops_(0),
+	bilinear_samples_(0),
+	flags_(0)
 	{}
 
 	SCapabilities(
-		ERogueGeneration g,
-		unsigned int fp16,
-		unsigned int fp32,
-		unsigned int bs,
-		unsigned int f
+		Family family,
+		unsigned int fp16_ops,
+		unsigned int fp32_ops,
+		unsigned int bilinear_samples,
+		unsigned int flags
 	):
-		generation(g),
-		fp16_operations_per_clock(fp16),
-		fp32_operations_per_clock(fp32),
-		bilinear_samples_per_clock(bs),
-		flags(f)
+		family_(family),
+		fp16_ops_(fp16_ops),
+		fp32_ops_(fp32_ops),
+		bilinear_samples_(bilinear_samples),
+		flags_(flags)
 		{}
 };
 	/*!****************************************************************************
-	 * @function        DetermineRogueCapabilities
-	 * @brief           Conservative estimate of a PowerVR Rogue GPU's capabilities.
-	 *                  "Han" and "Hood" cases handle legacy GL_RENDERER string formatting
+	 * @function        DetermineGPUCapabilities
+	 * @brief           Returns a struct of peak per-clock performance stats & 
+     *                  hardware capability flags for a given GL_RENDERER string
+	 *                  Note: "Han" and "Hood" cases handle legacy GL_RENDERER string formatting
 	 *
 	 * @param[in]       renderer_string   GL_RENDERER string value
 	 * @param[out]      capabilities      Struct containing GPU capabilities
 	 * @return          bool              True if the model number is recognized
 	 ******************************************************************************/
-	bool DetermineRogueCapabilities(const std::string& renderer_string, SCapabilities& capabilities) {
-		if (std::string::npos == renderer_string.find("Rogue")) {
-			return false;
-		}
+	static bool DetermineGPUCapabilities(const std::string& renderer_string, SCapabilities& capabilities) {
+        capabilities = {};
 
-		capabilities = {};
-
-		static const std::unordered_map<std::string, SCapabilities> caps {
-			// PowerVR Series6
-			{ "G6200",  { PVR_SERIES6, 128, 128, 4,  0                } },
-			{ "Han",    { PVR_SERIES6, 128, 128, 4,  0                } },
-			{ "G6230",  { PVR_SERIES6, 192, 128, 4,  PVR_CAP_FP16     } },
-			{ "G6400",  { PVR_SERIES6, 256, 256, 8,  0                } },
-			{ "Hood",   { PVR_SERIES6, 256, 256, 8,  0                } },
-			{ "G6430",  { PVR_SERIES6, 384, 256, 8,  PVR_CAP_FP16     } },
-			{ "G6630",  { PVR_SERIES6, 576, 384, 12, PVR_CAP_FP16     } },
-			// PowerVR  Series6XE
-			{ "G6020",  { PVR_SERIES6XE, 32, 8,  1,  PVR_CAP_FP16     } },
-			{ "G6050",  { PVR_SERIES6XE, 48, 32, 2,  PVR_CAP_FP16     } },
-			{ "G6060",  { PVR_SERIES6XE, 48, 32, 2,  PVR_CAP_FP16     } },
-			{ "G6100",  { PVR_SERIES6XE, 96, 64, 4,  PVR_CAP_FP16     } },
-			{ "G6110",  { PVR_SERIES6XE, 96, 64, 4,  PVR_CAP_FP16     } },
-			// PowerVR Series6XT
-			{ "GX6240", { PVR_SERIES6XT, 256, 128, 4,  PVR_CAP_FP16   } },
-			{ "GX6250", { PVR_SERIES6XT, 256, 128, 4,  PVR_CAP_FP16   } },
-			{ "GX6450", { PVR_SERIES6XT, 512, 256, 8,  PVR_CAP_FP16   } },
-			{ "GX6650", { PVR_SERIES6XT, 768, 384, 12, PVR_CAP_FP16   } },
-			// PowerVR Series7XE
-			{ "GE7400", { PVR_SERIES7XE, 48,  32,  2,  PVR_CAP_FP16   } },
-			{ "GE7800", { PVR_SERIES7XE, 96,  64,  4,  PVR_CAP_FP16   } },
-			// PowerVR Series7XT
-			{ "GT7200", { PVR_SERIES7XT, 256,  128,  4,  PVR_CAP_FP16 } },
-			{ "GT7400", { PVR_SERIES7XT, 512,  256,  8,  PVR_CAP_FP16 } },
-			{ "GT7600", { PVR_SERIES7XT, 768,  384,  12, PVR_CAP_FP16 } },
-			{ "GT7800", { PVR_SERIES7XT, 1024, 512,  16, PVR_CAP_FP16 } },
-			{ "GT7900", { PVR_SERIES7XT, 2048, 1024, 32, PVR_CAP_FP16 } },
+		static const std::unordered_map<std::string, SCapabilities> gpu_models {
+			// { "GPU model", { Family, Ops(FP16), Ops(FP32), Bilinear samples, Flags}},
+            // PowerVR Series6
+			{ "G6200",  { Family::SERIES6,   128,  128,  4,  0        } },
+            { "Han",    { Family::SERIES6,   128,  128,  4,  0        } }, // May be G6200 or G6230
+			{ "G6230",  { Family::SERIES6,   192,  128,  4,  CAP_FP16 } },
+			{ "G6400",  { Family::SERIES6,   256,  256,  8,  0        } },
+			{ "Hood",   { Family::SERIES6,   256,  256,  8,  0        } }, // May be G6400 or G6430
+			{ "G6430",  { Family::SERIES6,   384,  256,  8,  CAP_FP16 } },
+			{ "G6630",  { Family::SERIES6,   576,  384,  12, CAP_FP16 } },
+            // PowerVR Series6XE
+			{ "G6020",  { Family::SERIES6XE, 32,   8,    1,  CAP_FP16 } },
+			{ "G6050",  { Family::SERIES6XE, 48,   32,   2,  CAP_FP16 } },
+			{ "G6060",  { Family::SERIES6XE, 48,   32,   2,  CAP_FP16 } },
+			{ "G6100",  { Family::SERIES6XE, 96,   64,   4,  CAP_FP16 } },
+			{ "G6110",  { Family::SERIES6XE, 96,   64,   4,  CAP_FP16 } },
+            // PowerVR Series6XT
+			{ "GX6240", { Family::SERIES6XT, 256,  128,  4,  CAP_FP16 } },
+			{ "GX6250", { Family::SERIES6XT, 256,  128,  4,  CAP_FP16 } },
+			{ "GX6450", { Family::SERIES6XT, 512,  256,  8,  CAP_FP16 } },
+			{ "GX6650", { Family::SERIES6XT, 768,  384,  12, CAP_FP16 } },
+            // PowerVR Series7XE
+			{ "GE7400", { Family::SERIES7XE, 48,   32,   2,  CAP_FP16 } },
+			{ "GE7800", { Family::SERIES7XE, 96,   64,   4,  CAP_FP16 } },
+            // PowerVR Series7XT
+			{ "GT7200", { Family::SERIES7XT, 256,  128,  4,  CAP_FP16 } },
+			{ "GT7400", { Family::SERIES7XT, 512,  256,  8,  CAP_FP16 } },
+			{ "GT7600", { Family::SERIES7XT, 768,  384,  12, CAP_FP16 } },
+			{ "GT7800", { Family::SERIES7XT, 1024, 512,  16, CAP_FP16 } },
+			{ "GT7900", { Family::SERIES7XT, 2048, 1024, 32, CAP_FP16 } },
 			// PowerVR Wizard
-			{ "GR6500", { PVR_SERIES6XT, 512, 256, 8, PVR_CAP_FP16 | PVR_CAP_RAYTRACING } }
+			{ "GR6500", { Family::SERIES6XT, 512,  256,  8,  CAP_FP16 | CAP_RAYTRACING } }
 		};
 
-		for (const auto &kv : caps) {
-			if (renderer_string.find(kv.first) != std::string::npos) {
-				capabilities = kv.second;
+		for (const auto &model : gpu_models) {
+			if (renderer_string.find(model.first) != std::string::npos) {
+				capabilities = model.second;
 				return true;
 			}
 		}
