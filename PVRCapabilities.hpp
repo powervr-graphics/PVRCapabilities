d2202346a9d804424e12534afa7041368f7e1a94
Joe Davis <joe.davis@imgtec.com>
2015-04-21 14:06:19 +0100

    Feature: Added ShaderEngine enum

--- PVRCapabilities.hpp
+++ PVRCapabilities.hpp
@@ -14,13 +14,20 @@ enum class Family {
 	SERIES7XT,
 	UNKNOWN
 };
+enum class ShaderEngine {
+	USSE1,
+	USSE2,
+	USC,
+	UNKNOWN
+};
 enum EFlags {
 	CAP_FP16         = 1 << 1,      // USC has 16-bit float units
 	CAP_RAYTRACING   = 1 << 2,      // Ray tracing units 
 	CAP_TESSELLATION = 1 << 3       // Tessellation units 
 };
 struct SCapabilities {
-	Family family_;	                // PowerVR variant
+	Family family_;                 // PowerVR variant
+	ShaderEngine shader_engine_;    // Shader engine variant
 	unsigned int fp16_ops_;         // Peak operations per-clock (FP16)
 	unsigned int fp32_ops_;         // Peak operations per-clock (FP32)
 	unsigned int bilinear_samples_; // Peak bilinear samples per-clock
@@ -29,6 +36,7 @@ struct SCapabilities {
 	SCapabilities()
 	:
 	family_(Family::UNKNOWN),
+	shader_engine_(ShaderEngine::UNKNOWN),
 	fp16_ops_(0),
 	fp32_ops_(0),
 	bilinear_samples_(0),
@@ -37,12 +45,14 @@ struct SCapabilities {
 
 	SCapabilities(
 	Family family,
+	ShaderEngine shader_engine,
 	unsigned int fp16_ops,
 	unsigned int fp32_ops,
 	unsigned int bilinear_samples,
 	unsigned int flags
 	):
 	family_(family),
+	shader_engine_(shader_engine),
 	fp16_ops_(fp16_ops),
 	fp32_ops_(fp32_ops),
 	bilinear_samples_(bilinear_samples),
@@ -66,47 +76,47 @@ struct SCapabilities {
         capabilities = {};
 
 		static const std::unordered_map<std::string, SCapabilities> gpu_models {
-			// { "GPU model", { Family, Ops(FP16), Ops(FP32), Bilinear samples, Flags}},
+			// { "GPU model", { Family, ShaderEngine, Ops(FP16), Ops(FP32), Bilinear samples, Flags}},
 			// PowerVR Series5
-			{ "SGX 530", { Family::SERIES5,   8,    8,    1,  0        } },
-			{ "SGX 531", { Family::SERIES5,   8,    8,    1,  0        } },
-			{ "SGX 535", { Family::SERIES5,   8,    8,    2,  0        } },
-			{ "SGX 540", { Family::SERIES5,   16,   16,   2,  0        } },
-			{ "SGX 545", { Family::SERIES5,   16,   16,   2,  0        } },
+			{ "SGX 530", { Family::SERIES5,   ShaderEngine::USSE1, 8,    8,    1,  0        } },
+			{ "SGX 531", { Family::SERIES5,   ShaderEngine::USSE1, 8,    8,    1,  0        } },
+			{ "SGX 535", { Family::SERIES5,   ShaderEngine::USSE1, 8,    8,    2,  0        } },
+			{ "SGX 540", { Family::SERIES5,   ShaderEngine::USSE1, 16,   16,   2,  0        } },
+			{ "SGX 545", { Family::SERIES5,   ShaderEngine::USSE1, 16,   16,   2,  0        } },
 			// PowerVR Series5XT
-			{ "SGX 543", { Family::SERIES5XT, 32,   32,   2,  0        } },
-			{ "SGX 544", { Family::SERIES5XT, 32,   32,   2,  0        } },
-			{ "SGX 554", { Family::SERIES5XT, 64,   64,   2,  0        } },
+			{ "SGX 543", { Family::SERIES5XT, ShaderEngine::USSE2, 32,   32,   2,  0        } },
+			{ "SGX 544", { Family::SERIES5XT, ShaderEngine::USSE2, 32,   32,   2,  0        } },
+			{ "SGX 554", { Family::SERIES5XT, ShaderEngine::USSE2, 64,   64,   2,  0        } },
 			// PowerVR Series6
-			{ "G6200",  { Family::SERIES6,   128,  128,  4,  0        } },
-			{ "Han",    { Family::SERIES6,   128,  128,  4,  0        } }, // May be G6200 or G6230
-			{ "G6230",  { Family::SERIES6,   192,  128,  4,  CAP_FP16 } },
-			{ "G6400",  { Family::SERIES6,   256,  256,  8,  0        } },
-			{ "Hood",   { Family::SERIES6,   256,  256,  8,  0        } }, // May be G6400 or G6430
-			{ "G6430",  { Family::SERIES6,   384,  256,  8,  CAP_FP16 } },
-			{ "G6630",  { Family::SERIES6,   576,  384,  12, CAP_FP16 } },
+			{ "G6200",  { Family::SERIES6,    ShaderEngine::USC,   128,  128,  4,  0        } },
+			{ "Han",    { Family::SERIES6,    ShaderEngine::USC,   128,  128,  4,  0        } }, // May be G6200 or G6230
+			{ "G6230",  { Family::SERIES6,    ShaderEngine::USC,   192,  128,  4,  CAP_FP16 } },
+			{ "G6400",  { Family::SERIES6,    ShaderEngine::USC,   256,  256,  8,  0        } },
+			{ "Hood",   { Family::SERIES6,    ShaderEngine::USC,   256,  256,  8,  0        } }, // May be G6400 or G6430
+			{ "G6430",  { Family::SERIES6,    ShaderEngine::USC,   384,  256,  8,  CAP_FP16 } },
+			{ "G6630",  { Family::SERIES6,    ShaderEngine::USC,   576,  384,  12, CAP_FP16 } },
 			// PowerVR Series6XE
-			{ "G6020",  { Family::SERIES6XE, 32,   8,    1,  CAP_FP16 } },
-			{ "G6050",  { Family::SERIES6XE, 48,   32,   2,  CAP_FP16 } },
-			{ "G6060",  { Family::SERIES6XE, 48,   32,   2,  CAP_FP16 } },
-			{ "G6100",  { Family::SERIES6XE, 96,   64,   4,  CAP_FP16 } },
-			{ "G6110",  { Family::SERIES6XE, 96,   64,   4,  CAP_FP16 } },
+			{ "G6020",  { Family::SERIES6XE,  ShaderEngine::USC,   32,   8,    1,  CAP_FP16 } },
+			{ "G6050",  { Family::SERIES6XE,  ShaderEngine::USC,   48,   32,   2,  CAP_FP16 } },
+			{ "G6060",  { Family::SERIES6XE,  ShaderEngine::USC,   48,   32,   2,  CAP_FP16 } },
+			{ "G6100",  { Family::SERIES6XE,  ShaderEngine::USC,   96,   64,   4,  CAP_FP16 } },
+			{ "G6110",  { Family::SERIES6XE,  ShaderEngine::USC,   96,   64,   4,  CAP_FP16 } },
 			// PowerVR Series6XT
-			{ "GX6240", { Family::SERIES6XT, 256,  128,  4,  CAP_FP16 } },
-			{ "GX6250", { Family::SERIES6XT, 256,  128,  4,  CAP_FP16 } },
-			{ "GX6450", { Family::SERIES6XT, 512,  256,  8,  CAP_FP16 } },
-			{ "GX6650", { Family::SERIES6XT, 768,  384,  12, CAP_FP16 } },
+			{ "GX6240", { Family::SERIES6XT,  ShaderEngine::USC,   256,  128,  4,  CAP_FP16 } },
+			{ "GX6250", { Family::SERIES6XT,  ShaderEngine::USC,   256,  128,  4,  CAP_FP16 } },
+			{ "GX6450", { Family::SERIES6XT,  ShaderEngine::USC,   512,  256,  8,  CAP_FP16 } },
+			{ "GX6650", { Family::SERIES6XT,  ShaderEngine::USC,   768,  384,  12, CAP_FP16 } },
 			// PowerVR Series7XE
-			{ "GE7400", { Family::SERIES7XE, 48,   32,   2,  CAP_FP16 } },
-			{ "GE7800", { Family::SERIES7XE, 96,   64,   4,  CAP_FP16 } },
+			{ "GE7400", { Family::SERIES7XE,  ShaderEngine::USC,   48,   32,   2,  CAP_FP16 } },
+			{ "GE7800", { Family::SERIES7XE,  ShaderEngine::USC,   96,   64,   4,  CAP_FP16 } },
 			// PowerVR Series7XT
-			{ "GT7200", { Family::SERIES7XT, 256,  128,  4,  CAP_FP16 | CAP_TESSELLATION } },
-			{ "GT7400", { Family::SERIES7XT, 512,  256,  8,  CAP_FP16 | CAP_TESSELLATION } },
-			{ "GT7600", { Family::SERIES7XT, 768,  384,  12, CAP_FP16 | CAP_TESSELLATION } },
-			{ "GT7800", { Family::SERIES7XT, 1024, 512,  16, CAP_FP16 | CAP_TESSELLATION } },
-			{ "GT7900", { Family::SERIES7XT, 2048, 1024, 32, CAP_FP16 | CAP_TESSELLATION } },
+			{ "GT7200", { Family::SERIES7XT,  ShaderEngine::USC,   256,  128,  4,  CAP_FP16 | CAP_TESSELLATION } },
+			{ "GT7400", { Family::SERIES7XT,  ShaderEngine::USC,   512,  256,  8,  CAP_FP16 | CAP_TESSELLATION } },
+			{ "GT7600", { Family::SERIES7XT,  ShaderEngine::USC,   768,  384,  12, CAP_FP16 | CAP_TESSELLATION } },
+			{ "GT7800", { Family::SERIES7XT,  ShaderEngine::USC,   1024, 512,  16, CAP_FP16 | CAP_TESSELLATION } },
+			{ "GT7900", { Family::SERIES7XT,  ShaderEngine::USC,   2048, 1024, 32, CAP_FP16 | CAP_TESSELLATION } },
 			// PowerVR Wizard
-			{ "GR6500", { Family::SERIES6XT, 512,  256,  8,  CAP_FP16 | CAP_RAYTRACING } }
+			{ "GR6500", { Family::SERIES6XT,  ShaderEngine::USC,   512,  256,  8,  CAP_FP16 | CAP_RAYTRACING } }
 		};
 
 		for (const auto &model : gpu_models) {
